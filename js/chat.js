81f4212f50e4dfbc890553ca1918e60e9ef799ca
Bashirkwadir <bashirwk1998@gmail.com>
2025-03-20 21:26:59 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,4 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="3383c392-b3ad-4027-acc8-cff874f97b1d";
+(function(){d=document;s=d.createElement("script");
+  s.src="https://client.crisp.chat/l.js";
+  s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
